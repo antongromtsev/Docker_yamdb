c6d3d54c9dfeabf401e309e05f362d121a26c798
fenix <fenix217grom@gmail.com>
2021-08-24 21:52:44 +0300

    Расширил админку

--- api/admin.py
+++ api/admin.py
@@ -2,42 +2,53 @@ from django.contrib import admin
 
 from .models.users import MyUser
 from .models.titles import Title
+from .models.categories import Category
+from .models.comments import Comments
+from .models.ganres import Genre
+from .models.review import Review
 
 
 class MyUserAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "bio", "username", "role")
     search_fields = ("role",)
     list_filter = ("username",)
-    # empty_value_display = "-пусто-"
+    empty_value_display = "-пусто-"
 
 
-# class GroupAdmin(admin.ModelAdmin):
-#     list_display = ("pk", "title", "slug", "description")
-#     search_fields = ("description",)
-#     list_filter = ("slug",)
-#     empty_value_display = "-пусто-"
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    list_filter = ("slug",)
+    empty_value_display = "-пусто-"
 
 
 class TitleAdmin(admin.ModelAdmin):
     list_display = ("name", "year", "description")
     search_fields = ("description",)
     list_filter = ("year",)
-    # empty_value_display = "-пусто-"
+    empty_value_display = "-пусто-"
 
 
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ("pk", "text", "created", "author", "post")
-#     search_fields = ("text",)
-#     list_filter = ("created",)
-#     empty_value_display = "-пусто-"
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ("review", "text", "author", "pub_date")
+    search_fields = ("text",)
+    list_filter = ("pub_date",)
+    empty_value_display = "-пусто-"
 
 
-# class FollowAdmin(admin.ModelAdmin):
-#     list_display = ("author", "user")
-#     list_filter = ("author",)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    list_filter = ("slug",)
 
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("title", "text", "author", "score", "pub_date")
+    search_fields = ("author",)
+    list_filter = ("pub_date",)
+    empty_value_display = "-пусто-"
 
 admin.site.register(MyUser, MyUserAdmin)
 admin.site.register(Title, TitleAdmin)
-# admin.site.register(Group, GroupAdmin)
-# admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comments, CommentsAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Review, ReviewAdmin)
